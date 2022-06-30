fe930ce8cf0f648c546ee21844282d8c55218aab
Bhaumik T <106333136+bhaumik-tripathi@users.noreply.github.com>
2022-06-30 06:06:20 +0000

    Added templates and ststic

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Templetes')
+
+jobs = [{
+  "title": 'data analyst',
+  'salary': 1000000,
+  "location": "Bengaluru, India",
+  'Currency': 'Rs.'
+},
+       {
+  "title": 'data scientist',
+  'salary': 1500000,
+  "location": "Delhi, India",
+  'Currency': 'Rs.'
+},
+       {
+  "title": 'Frontend Engineer',
+  'salary': 20000,
+  "location": "Bengaluru, India",
+  'Currency': '$'
+},
+       {
+  "title": 'Backend engineer',
+  'salary': 50000,
+  "location": "Bengaluru, India",
+  'Currency':'$'
+}]
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', 
+                           Jobs=jobs,
+                           company_name='Jovian')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
